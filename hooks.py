b0da2a5a8fb963e9f95c740c34a6f855d413ac32
Oleh Prypin <oleh@pryp.in>
2022-01-05 11:39:09 +0200

    Revert "Use the version of Crystal according to the current branch" (#575)

--- hooks.py
+++ hooks.py
@@ -1,10 +1,7 @@
 import logging
-import os
 import re
 
-
 log = logging.getLogger('mkdocs')
-version = os.getenv('CRYSTAL_VERSION', 'latest')
 
 
 def on_page_markdown(markdown, page, **kwargs):
@@ -22,9 +19,3 @@ def on_page_markdown(markdown, page, **kwargs):
             f"Documentation file '{path}' contains a version-pinned link to the API: '{bad}'\n"
             f"Suggested fix: '{good}'"
         )
-
-    return re.sub(
-        r'\bhttps?://(www\.)?crystal-lang\.org/api/(?!([0-9]+(\.[0-9]+)+|latest|master)/)',
-        f'https://crystal-lang.org/api/{version}/',
-        markdown
-    )
