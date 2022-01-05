413f90fbb47446de0d4b63516486441f1873480d
Oleh Prypin <oleh@pryp.in>
2022-01-05 17:16:14 +0200

    Reland: Use the version of Crystal according to the current branch (#574)

--- hooks.py
+++ hooks.py
@@ -1,7 +1,10 @@
 import logging
+import os
 import re
 
+
 log = logging.getLogger('mkdocs')
+version = os.getenv('CRYSTAL_VERSION', 'master')
 
 
 def on_page_markdown(markdown, page, **kwargs):
@@ -19,3 +22,9 @@ def on_page_markdown(markdown, page, **kwargs):
             f"Documentation file '{path}' contains a version-pinned link to the API: '{bad}'\n"
             f"Suggested fix: '{good}'"
         )
+
+    return re.sub(
+        r'\bhttps?://(www\.)?crystal-lang\.org/api/(?!([0-9]+(\.[0-9]+)+|latest|master)/)',
+        f'https://crystal-lang.org/api/{version}/',
+        markdown
+    )
