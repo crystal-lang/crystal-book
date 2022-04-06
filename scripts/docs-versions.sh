f79332defe65adfd3d733316774b7f9bc740afca
Oleh Prypin <oleh@pryp.in>
2022-04-06 21:42:06 +0200

    Version selection: add AWS redirect configs for latest docs version (#561)
    Co-authored-by: Matías García Isaía <mgarcia@manas.tech>
    Co-authored-by: Johannes Müller <straightshoota@gmail.com>

--- scripts/docs-versions.sh
+++ scripts/docs-versions.sh
@@ -1,12 +1,19 @@
-#!/usr/bin/env sh
-set -e
+#!/bin/bash
+set -eu
 
-printf '['
-latest='"latest"'
-for ver in $(git ls-remote --heads "${1:-.}" | grep -P -o '(?<=refs/heads/release/)[0-9][0-9.]+' | sort -V -r); do
-  printf '{"version": "%s", "title": "%s", "aliases": [%s]}, ' "$ver" "$ver" "$latest"
-  latest=''
-done
-printf '{"version": "master", "title": "nightly", "aliases": []}'
-printf ']'
-test -z "$latest"  # Check that we wrote at least one version
+branches=( $(git ls-remote --heads "${1:-.}" | grep -P -o '(?<=refs/heads/release/)[0-9][0-9.]+' | sort -V -r) )
+latest_version="${branches[0]}"
+
+{
+  printf '[\n'
+  latest='"latest"'
+  for ver in "${branches[@]}"; do
+    printf '{"version": "%s", "title": "%s", "aliases": [%s]},\n' "$ver" "$ver" "$latest"
+    latest=''
+  done
+  printf '{"version": "master", "title": "nightly", "aliases": []}\n'
+  printf ']\n'
+  test -z "$latest"  # Check that we wrote at least one version
+} > versions.json
+
+sed 's/\${LATEST_VERSION}/'"${latest_version}"'/g' "$(dirname "$0")/aws-config.json" > aws-config.json
