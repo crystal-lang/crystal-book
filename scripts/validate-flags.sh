3451a7ddb6df236c3ae17f21930b02d00ee08245
Johannes Müller <straightshoota@gmail.com>
2024-11-07 10:55:25 +0100

    Download Crystal source explicitly, use latest compiler

--- scripts/validate-flags.sh
+++ scripts/validate-flags.sh
@@ -3,8 +3,12 @@
 set -eu
 
 CRYSTAL=${CRYSTAL:-crystal}
-CRYSTAL_PATH="$($CRYSTAL env CRYSTAL_PATH)"
-STDLIB_SRC="${CRYSTAL_PATH#*:}" # stdlib source directory is the last entry in CRYSTAL_PATH
+if [ -z "${STDLIB_SRC-}" ]; then
+  CRYSTAL_PATH="$($CRYSTAL env CRYSTAL_PATH)"
+  STDLIB_SRC="${CRYSTAL_PATH#*:}" # stdlib source directory is the last entry in CRYSTAL_PATH
+fi
+
+echo "Validating flags for stdlib source ${STDLIB_SRC} ($(cat "${STDLIB_SRC}/VERSION"))"
 
 filter_missing() {
   missing=0
