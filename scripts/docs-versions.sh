385794128f3d5b9722f0ca3d8b4ed68fcaf708aa
Johannes Müller <straightshoota@gmail.com>
2026-03-10 12:31:03 +0100

    Fix shellcheck SC2207

--- scripts/docs-versions.sh
+++ scripts/docs-versions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eu
 
-branches=( $(git ls-remote --heads "${1:-.}" | grep -P -o '(?<=refs/heads/release/)[0-9][0-9.]+' | sort -V -r) )
+mapfile -t branches < <(git ls-remote --heads "${1:-.}" | grep -P -o '(?<=refs/heads/release/)[0-9][0-9.]+' | sort -V -r)
 latest_version="${branches[0]}"
 
 {
