afcbbe05d1f23937959c7731a339424b70e287ab
Johannes Müller <straightshoota@gmail.com>
2026-03-10 12:32:54 +0100

    Add check-vcs-permalinks to git-hooks

--- devenv.nix
+++ devenv.nix
@@ -21,6 +21,7 @@
   git-hooks.hooks = {
     actionlint.enable = true;
     check-toml.enable = true;
+    check-vcs-permalinks.enable = true;
     markdownlint.enable = true;
     shellcheck = {
       enable = true;
