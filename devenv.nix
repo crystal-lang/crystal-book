00a06ab0e37d54b238f6e6577d3ba23235e9642f
Johannes Müller <straightshoota@gmail.com>
2026-03-13 12:15:01 +0100

    Add more git hooks (#892)
    Add git-hooks for:
    
    - actionlint
    - check-vcs-permalinks
    - shellcheck
    - zizmor (fixup for #886)
    
    And fixes outstanding violations.

--- devenv.nix
+++ devenv.nix
@@ -19,8 +19,17 @@
   };
 
   git-hooks.hooks = {
+    actionlint.enable = true;
     check-toml.enable = true;
+    check-vcs-permalinks.enable = true;
     markdownlint.enable = true;
+    shellcheck = {
+      enable = true;
+      excludes = [
+        ".*\.zsh$"
+      ];
+    };
     typos.enable = true;
+    zizmor.enable = true;
   };
 }
